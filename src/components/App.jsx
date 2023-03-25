e4f576bd326a80629294cd2fcfcbd4a4fed88c11
Den <52295780+josserden@users.noreply.github.com>
2023-03-25 11:29:00 +0200

    add base layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,25 +24,33 @@ export class App extends Component {
         </header>
 
         <section className="py-20">
-          <div className="container">
+          <div className="container grid grid-cols-[400px_1fr] gap-10">
             <form onSubmit={this.handleFormSubmit}>
-              <input
-                type="text"
-                className="input"
-                name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-              />
+              <div className="mb-10 grid gap-5">
+                <label className="grid gap-2.5">
+                  <span className="font-bold text-indigo-800">Name</span>
+                  <input
+                    type="text"
+                    className="input"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                  />
+                </label>
 
-              <input
-                type="tel"
-                className="input"
-                name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-              />
+                <label className="grid gap-2.5">
+                  <span className="font-bold text-indigo-800">Number</span>
+                  <input
+                    type="tel"
+                    className="input"
+                    name="number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                  />
+                </label>
+              </div>
 
               <button
                 className="text-md w-full max-w-[260px] rounded-md bg-indigo-600 px-3 py-2.5 font-bold text-white shadow-md transition-colors hover:bg-indigo-400"
